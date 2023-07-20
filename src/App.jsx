a5258f9d9ddaf2c12f9935977f1ed4bf0aad14b4
MagNeSioO14 <boris729@hotmail.com>
2023-07-19 21:37:17 -0300

    Elimine el import del app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from "./componentes/Header/Header"
-import './app.css'
 import ItemsListContainer from "./componentes/ItemListContainer/ItemListContainer"
 import './componentes/Header/Header.scss'
 import './componentes/ItemListContainer/ItemListContainer.scss'
