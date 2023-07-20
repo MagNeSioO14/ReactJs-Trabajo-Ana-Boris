28ce8832b7d5cdbf6836611f9bf0f73616b85ff9
MagNeSioO14 <boris729@hotmail.com>
2023-07-19 21:13:49 -0300

    Corregi unas imagenes

--- src/componentes/Carousel/CarouselDark.jsx
+++ src/componentes/Carousel/CarouselDark.jsx
@@ -6,13 +6,13 @@ export const CarouselDark = () => {
     return (
         <Carousel data-bs-theme="dark" className='carouselEstilos'>
             <Carousel.Item>
-                <img src="../../public/carousel/1.png" alt="" />
+                <img src="../../carousel/1.png" alt="" />
             </Carousel.Item>
             <Carousel.Item>
-                <img src="../../public/carousel/23.png" alt="" />
+                <img src="../../carousel/23.png" alt="" />
             </Carousel.Item>
             <Carousel.Item>
-                <img src="../../public/carousel/remeras 2.jpg" alt="" />
+                <img src="../../carousel/remeras 2.jpg" alt="" />
             </Carousel.Item>
 
         </Carousel>
